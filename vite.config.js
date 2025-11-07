355b33d28c5e0951716c57dd7bb7672a3cfd7179
Aleksandr Frolov <fr.28.09.07@gmail.com>
2025-11-08 00:48:52 +0100

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/VLAD-PAVLOV/", // имя репозитория
   css: {
     preprocessorOptions: {
-      less: {
-        javascriptEnabled: true, // нужно для некоторых библиотек Less
-      },
+      less: { javascriptEnabled: true },
     },
   },
   build: {
-    outDir: "dist",
-    emptyOutDir: true, // очищает dist перед сборкой
+    outDir: "docs", // <- собирать прямо в /docs для GitHub Pages
+    emptyOutDir: true,
   },
-  base: "/VLAD-PAVLOV/", // важно для GitHub Pages
 });
