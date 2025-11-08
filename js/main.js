87cd49fb4e7d835905f1c6196f7e0edbb83c8871
Aleksandr Frolov <fr.28.09.07@gmail.com>
2025-11-08 01:15:00 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-import "../less/style.less";
+import "../less/style.less"; // как у тебя было
+
+import "./onload-animate.js";
+import "./file-change.js";
+import "./open-form.js";
+import "./change-phone.js";
+import "./workers-tabs.js";
+import "./service-tabs.js";
+import "./animation.js";
+import "./slider.js";
+import "./form-validation.js";
+import "./goup.js";
